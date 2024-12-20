ef4ab7cc7b97a59e73dd980c7b65401caa011731
Richard Seaman <richard.seaman@coolplanet.io>
2024-12-20 15:45:00 +0000

    Add real tools.

---
+++ src/agent.ts
@@ -0,0 +1,51 @@
+import type { AIMessage } from '../types'
+import { runLLM } from './llm'
+// import { z } from 'zod'
+// import { runTool } from './toolRunner'
+import { addMessages, getMessages, saveToolResponse } from './memory'
+import { runTool } from './toolRunner'
+import { logMessage, showLoader } from './ui'
+
+export const runAgent = async ({
+  userMessage,
+  tools,
+}: {
+  userMessage: string
+  tools: any[]
+}) => {
+  await addMessages([
+    {
+      role: 'user',
+      content: userMessage,
+    },
+  ])
+
+  const loader = showLoader('Thinking...')
+
+  while (true) {
+    const history = await getMessages()
+    const response = await runLLM({
+      messages: history,
+      tools,
+    })
+
+    await addMessages([response])
+
+    logMessage(response)
+
+    if (response.content) {
+      loader.stop()
+      return getMessages()
+    }
+
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0]
+      loader.update(`executing: ${toolCall.function.name}`)
+
+      const toolResponse = await runTool(toolCall, userMessage)
+      await saveToolResponse(toolCall.id, toolResponse)
+
+      loader.update(`executed: ${toolCall.function.name}`)
+    }
+  }
+}
--- src/llm.ts
+++ src/llm.ts
@@ -1,19 +1,27 @@
 import type { AIMessage } from '../types'
 import { openai, type ChatModelStr } from './ai'
+import { zodFunction } from 'openai/helpers/zod'
+import { z } from 'zod'
 
 export const runLLM = async ({
   model = 'gpt-4o-mini',
   messages,
+  tools,
   temperature = 0.1,
 }: {
   messages: AIMessage[]
+  tools?: { name: string; parameters: z.AnyZodObject }[]
   temperature?: number
   model?: ChatModelStr
 }) => {
+  const formattedTools = tools?.map(zodFunction)
   const response = await openai.chat.completions.create({
     model,
     messages,
     temperature,
+    tools: formattedTools,
+    tool_choice: 'auto',
+    parallel_tool_calls: false,
   })
   return response.choices[0].message
 }
